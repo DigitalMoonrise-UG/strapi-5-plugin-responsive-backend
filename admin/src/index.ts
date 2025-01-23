78e7104dc9985cb3d4b4972177afd70df3f690e2
Naegato <maxime.wiatr@gmail.com>
2025-01-23 15:33:40 +0100

    feat/content-type-builder-and-media-library

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -13,13 +13,26 @@ export default {
 		const style = document.createElement('style');
 		style.textContent = `
         @media (max-width: 767px) {
-            div[data-strapi-header] {
+        	div:has(> div[data-strapi-header]) {
+        		height: unset !important;
+			}
+
+            div[data-strapi-header], div:has(> div[data-strapi-header]) + div {
                 padding: 20px;
             }
-            #main-content > div:last-child {
+
+            div:has(> div[data-strapi-header]) + div {
+            	flex-wrap: wrap;
+			}
+
+            div[data-strapi-header] > div:last-of-type, div[data-strapi-header] > div:last-of-type * {
+                flex-wrap: wrap;
+            }
+
+            #main-content:not(:has(main)) > div:last-child {
                 padding: 20px;
             }
-            nav[aria-label="Content Manager"] {
+            nav[aria-label="Content Manager"], nav[aria-label="Content-Type Builder"]{
                 width: 100%;
                 height: auto;
                 position: inherit;
@@ -31,11 +44,33 @@ export default {
             a[href="#main-content"] + div > div > div {
 	            grid-template-columns: repeat(1, minmax(0, 1fr));
             }
-            main[aria-labelledby="main-content-title"] {
+
+            main[aria-labelledby="main-content-title"]:not(:has(div[data-strapi-header="true"], div[data-strapi-header-sticky="true"]))
                 padding: 20px;
             }
+
+            div[id="radix-:rp:-content-draft"] > div > div > div > div {
+            	gap: unset;
+			}
+
+			div[data-strapi-header-sticky] {
+				width: calc(100% - 55px);
+			}
+
+			div[data-strapi-header-sticky] > div, div[data-strapi-header-sticky] > div > div > div, div[data-strapi-header-sticky] > div > div > div > div {
+				flex-wrap: wrap;
+				gap: 10px;
+				padding: 0;
+			}
+
+			body[data-scroll-locked] > div:last-of-type > div {
+				width: 90%;
+			}
        }
 	`;
 		document.head.appendChild(style);
 	},
 };
+
+// <main aria-labelledby="main-content-title" id="main-content" tabindex="-1" class="sc-KXCwU hmVCXt sc-fDEMvA bRwPdd">
+// <main aria-labelledby="main-content-title" id="main-content" tabindex="-1" class="sc-KXCwU geZizU sc-fDEMvA bRwPdd">
